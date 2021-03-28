3cd0f5d626eea2d82174945d88ef8c78d0aa2ba4
Zoryana Sobol <Zoryana.Sobol@jumar-technology.com>
2021-03-28 22:53:06 +0300

    add

---
+++ MessagesService/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MessagesService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
